8395176f63c43b0ef63d8b710557f5be5a8964d9
Saumya Chaturvedi <saumyachat03@gmail.com>
2024-10-03 19:14:34 +0530

    required packages and clones for eda

---
+++ script.sh
@@ -0,0 +1,9 @@
+pip install -q torch -f https://download.pytorch.org/whl/cu101/torch_stable.html
+pip install -q transformers==3.5.0 fast-trees
+pip install -q datasets
+pip install bs4
+pip install pandas
+pip install tqdm
+git clone -q https://github.com/microsoft/CodeXGLUE.git
+wget https://zenodo.org/record/7857872/files/python.zip
+unzip python.zip
