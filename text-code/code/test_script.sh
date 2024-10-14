dfe136e8333808bcd36c18d3728c7f251db7140a
shollercoaster <saumyachat03@gmail.com>
2024-10-14 14:03:48 +0200

    evaluator/

---
+++ text-code/code/test_script.sh
@@ -0,0 +1,21 @@
+cd code
+python run_lora.py \
+    --output_dir=../saved_models \
+    --model_type=roberta \
+    --config_name=microsoft/graphcodebert-base \
+    --model_name_or_path=microsoft/graphcodebert-base \
+    --tokenizer_name=microsoft/graphcodebert-base \
+    --do_eval \
+    --do_test \
+    --train_data_file=../dataset/train.jsonl \
+    --eval_data_file=../dataset/valid.jsonl \
+    --test_data_file=../dataset/test.jsonl \
+    --fp16_opt_level=O1 \
+    --epoch 2 \
+    --block_size 256 \
+    --train_batch_size 64 \
+    --eval_batch_size 64 \
+    --learning_rate 5e-5 \
+    --max_grad_norm 1.0 \
+    --evaluate_during_training \
+    --seed 123456 2>&1| tee test.log
